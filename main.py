953394a15ebe942e1016cf4b798c16e561cad0ac
Mikachan456 <mikaelaranpo456@gmail.com>
2025-07-05 17:48:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import cog
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="YOUR PREFIX HERE", intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
 
 bot.add_cog(cog.Mudae(bot))
 
-bot.run("YOUR TOKEN HERE")
+bot.run("8")
